baba54ffffe6e9188a9a8a7b253a41f6fce4705d
Nate Gibbons <nate.gibbons@gmail.com>
2021-09-23 16:33:18 -0600

    filtered out index.md

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     md_files
         .into_iter()
         .map(|file| parser.process_todos(file))
-        .filter(|tf| tf.has_todos())
+        .filter(|tf| tf.has_todos() && tf.filename != "index.md")
         .for_each(|tf| {
             println!("{}", tf.filename.bold().bright_cyan());
             for item in tf.items {
